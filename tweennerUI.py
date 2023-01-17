1ee79817b579de7af02926b80523c39dc0590a3a
tahersasta <122855424+tahersasta@users.noreply.github.com>
2023-01-17 09:47:06 -0500

    Update tweennerUI.py

--- tweennerUI.py
+++ tweennerUI.py
@@ -50,23 +50,3 @@ def tween(percentage, obj=None, attrs=None, selection=True):
         cmds.setAttr(attrFull, currentValue)
         cmds.setKeyframe(attrFull, time=currentTime, value=currentValue)
 
-class TweenerWindow(object):
-    windowName = "TweenerWindow"
-    def show(self):
-        if cmds.window(self.windowName, query=True, exists=True):
-            cmds.deleteUI(self.windowName)
-        cmds.window(self.windowName)
-        self.buildUI()
-        cmds.showWindow()
-    def buildUI(self):
-        column = cmds.columnLayout()
-        cmds.text(label="Use this slider to set the tween amount")
-        row = cmds.rowLayout(numberOfColumns=2)
-        self.slider = cmds.floatSlider(min=0, max=100, value=50, step=1, changeCommand=tween)
-        cmds.button(label="Reset", command=self.reset)
-        cmds.setParent(column)
-        cmds.button(label="Close", command=self.close)
-    def reset(self, *args):
-        cmds.floatSlider(self.slider, edit=True, value=50)
-    def close(self, *args):
-        cmds.deleteUI(self.windowName)
